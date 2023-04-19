3d5e1b4d1ca4ebe8c844a3948e45325acd221f5c
Archik-K <ar.kryatova@yandex.ru>
2023-04-20 00:02:03 +0300

    first commit

--- index1.js
+++ index1.js
@@ -1,31 +1,46 @@
 function transformName() {
-	const nameInput = document.getElementById("name");
-	let name = nameInput.value.trim().toLowerCase();
-	// Используем метод capitalize(), чтобы привести первую букву имени к верхнему регистру:
+	const nameInput = document.getElementById("name"); //получили айди инпута фио
+	let name = nameInput.value.trim().toLowerCase(); //убрали лишние пробелы и превели всё в верхний регистр
+	// Используем метод capitalize(), чтобы привести первую букву имени к верхнему регистру
 	name = capitalize(name);
 	return name;
 }
-
 function capitalize(str) {
 	return str.charAt(0).toUpperCase() + str.slice(1);
 }
-/*   В этой функцие `transformName()`, мы добавляем вспомогательную функцию `capitalize()`, 
-  которая занимается приведением первой буквы строки к верхнему регистру. Затем мы используем `capitalize()` 
+/*   В этой функции `transformName()`, добавляем вспомогательную функцию `capitalize()`, 
+  которая занимается приведением первой буквы строки к верхнему регистру. Затем используем `capitalize()` 
   для преобразования первой буквы имени в верхний регистр. */
 
+/* Для реализации функции `checkSpam(str)` можно использовать метод `replace()` для замены всех вхождений "viagra" и "XXX" на "***": */
+function checkSpam(str) {
+	return str.replace(/viagra|XXX/gi, "***");
+}
+/* Здесь использовали регулярное выражение `/viagra|XXX/gi` для поиска всех вхождений "viagra" и "XXX" в строке `str` с учётом регистра. 
+Метод `replace()` заменяет найденные вхождения на "***" и возвращает новую строку. */
+
+//элементы формы
 const nameInput = document.getElementById("name");
 const Avatarmessage = document.getElementById("avatar");
-const messagesDiv = document.getElementById("messages");
 const messageInput = document.getElementById("message-input");
+const messagesDiv = document.getElementById("messages");
 const sendButton = document.getElementById("send-button");
 sendButton.addEventListener("click", () => {
 	const name = nameInput.value;
 	const avatar = Avatarmessage.value;
 	const message = messageInput.value;
 	const messageElement = document.createElement("div");
-	messageElement.textContent = name + " " + avatar + " " + message;
+	messageElement.innerHTML = `<img src="${avatar}" alt='изображение аватара'><p> ${name}<p/><p>${message}</p>`;
 	messagesDiv.appendChild(messageElement);
 	messageInput.value = "";
 	Avatarmessage.value = "";
 	nameInput.value = "";
 });
+/* Этот код создает пять переменных, которые содержат ссылки на HTML-элементы: 
+'nameInput' - для поля ввода ФИО, 
+'Avatarmessage' - для поля ввода ссылки на картинку, 
+'messageInput' - для текстового поля ввода комментария, 
+'messagesDiv' - для контейнера вывода всех элементов, 
+'sendButton' - для кнопки отправки сообщения. 
+Затем добавляется обработчик события "click" для кнопки отправки сообщения, который создает 
+новый HTML-элемент для сообщения, добавляет его в контейнер сообщений и очищает текстовое поле ввода. */
