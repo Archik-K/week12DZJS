ce887380dea148ce973fecc5233272c3010409fb
Archik-K <ar.kryatova@yandex.ru>
2023-04-20 17:15:43 +0300

    first commit

--- index1.js
+++ index1.js
@@ -1,36 +1,43 @@
-function transformName() {
-	const nameInput = document.getElementById("name"); //получили айди инпута фио
-	let name = nameInput.value.trim().toLowerCase(); //убрали лишние пробелы и превели всё в верхний регистр
-	// Используем метод capitalize(), чтобы привести первую букву имени к верхнему регистру
-	name = capitalize(name);
-	return name;
-}
-function capitalize(str) {
-	return str.charAt(0).toUpperCase() + str.slice(1);
-}
-/*   В этой функции `transformName()`, добавляем вспомогательную функцию `capitalize()`, 
-  которая занимается приведением первой буквы строки к верхнему регистру. Затем используем `capitalize()` 
-  для преобразования первой буквы имени в верхний регистр. */
-
-/* Для реализации функции `checkSpam(str)` можно использовать метод `replace()` для замены всех вхождений "viagra" и "XXX" на "***": */
-function checkSpam(str) {
-	return str.replace(/viagra|XXX/gi, "***");
-}
-/* Здесь использовали регулярное выражение `/viagra|XXX/gi` для поиска всех вхождений "viagra" и "XXX" в строке `str` с учётом регистра. 
-Метод `replace()` заменяет найденные вхождения на "***" и возвращает новую строку. */
-
 //элементы формы
 const nameInput = document.getElementById("name");
 const Avatarmessage = document.getElementById("avatar");
 const messageInput = document.getElementById("message-input");
 const messagesDiv = document.getElementById("messages");
 const sendButton = document.getElementById("send-button");
+/*   В этой функции `transformName()`, добавляем вспомогательную функцию `capitalize()`, 
+  которая занимается приведением первой буквы строки к верхнему регистру. Затем используем `capitalize()` 
+  для преобразования первой буквы имени в верхний регистр. */
+function transformName() {
+	const nameInput = document.getElementById("name");
+	let name = nameInput.value.trim().toLowerCase();
+	name = capitalize(name);
+	return name;
+}
+function capitalize(str) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+}
+const transformedName = transformName();
+messagesDiv.textContent = transformedName;
+
+/* Для реализации функции `checkSpam(str)` можно использовать метод `replace()` для замены всех вхождений "viagra" и "XXX" на "***": */
+
+function checkSpam(str) {
+	let spamtext = str.toLowerCase();
+	return spamtext.replace(/(viagra|xxx)/gi, "***");
+}
+
+const totalStringCheckSpam = checkSpam();
+document.getElementById("messages").innerHTML = totalStringCheckSpam;
+
+/* Здесь использовали регулярное выражение `/viagra|XXX/gi` для поиска всех вхождений "viagra" и "XXX" в строке `str` с учётом регистра. 
+Метод `replace()` заменяет найденные вхождения на "***" и возвращает новую строку. */
+
 sendButton.addEventListener("click", () => {
 	const name = nameInput.value;
 	const avatar = Avatarmessage.value;
 	const message = messageInput.value;
 	const messageElement = document.createElement("div");
-	messageElement.innerHTML = `<img src="${avatar}" alt='изображение аватара'><p> ${name}<p/><p>${message}</p>`;
+	messageElement.innerHTML = `<img src="${avatar}" alt='изображение аватара'><p> ${name}<br>${message}</p>`;
 	messagesDiv.appendChild(messageElement);
 	messageInput.value = "";
 	Avatarmessage.value = "";
