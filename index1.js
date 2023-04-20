04339da60749b0343c0e4516dba661494267a18e
Archik-K <ar.kryatova@yandex.ru>
2023-04-20 19:07:53 +0300

    first commit

--- index1.js
+++ index1.js
@@ -1,5 +1,5 @@
 //элементы формы
-const nameInput = document.getElementById("name");
+const nameInput = document.getElementById("nameFIO");
 const Avatarmessage = document.getElementById("avatar");
 const messageInput = document.getElementById("message-input");
 const messagesDiv = document.getElementById("messages");
@@ -7,36 +7,36 @@ const sendButton = document.getElementById("send-button");
 /*   В этой функции `transformName()`, добавляем вспомогательную функцию `capitalize()`, 
   которая занимается приведением первой буквы строки к верхнему регистру. Затем используем `capitalize()` 
   для преобразования первой буквы имени в верхний регистр. */
-function transformName() {
-	const nameInput = document.getElementById("name");
-	let name = nameInput.value.trim().toLowerCase();
+function transformName(name) {
+	name = name.trim().toLowerCase();
 	name = capitalize(name);
 	return name;
 }
 function capitalize(str) {
-	return str.charAt(0).toUpperCase() + str.slice(1);
+	let res = "";
+	let words = str.split(/[ ]+/);
+	let word;
+	for (word of words) // перебираем вводимые слова в переменной
+		res = res + word.charAt(0).toUpperCase() + word.slice(1) + " ";
+	return res;
 }
-const transformedName = transformName();
-messagesDiv.textContent = transformedName;
 
 /* Для реализации функции `checkSpam(str)` можно использовать метод `replace()` для замены всех вхождений "viagra" и "XXX" на "***": */
 
 function checkSpam(str) {
-	let spamtext = str.toLowerCase();
-	return spamtext.replace(/(viagra|xxx)/gi, "***");
+	let spamtext = str.toLowerCase(); //перевели в нижний регистр
+	return spamtext.replace(/viagra|xxx/gi, "***");
 }
-
-const totalStringCheckSpam = checkSpam();
-document.getElementById("messages").innerHTML = totalStringCheckSpam;
 
 /* Здесь использовали регулярное выражение `/viagra|XXX/gi` для поиска всех вхождений "viagra" и "XXX" в строке `str` с учётом регистра. 
 Метод `replace()` заменяет найденные вхождения на "***" и возвращает новую строку. */
 
 sendButton.addEventListener("click", () => {
-	const name = nameInput.value;
+	const name = transformName(nameInput.value);
 	const avatar = Avatarmessage.value;
-	const message = messageInput.value;
+	/* 	const message = messageInput.value; */
 	const messageElement = document.createElement("div");
+	const message = checkSpam(messageInput.value);
 	messageElement.innerHTML = `<img src="${avatar}" alt='изображение аватара'><p> ${name}<br>${message}</p>`;
 	messagesDiv.appendChild(messageElement);
 	messageInput.value = "";
